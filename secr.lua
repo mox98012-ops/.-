80db57ba4e41ebf0a4ea133704bbb0170391526d
mox98012-ops <mox98012@gmail.com>
2026-03-09 20:39:06 +0200

    Update loading mechanism for scripts in secr.lua

--- secr.lua
+++ secr.lua
@@ -1,6 +1,9 @@
+if (getgenv().serenium_LOADED) then 
+	return;
+end;
 info = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId);
 if (info.Creator.CreatorType == "Group" or info.Creator.CreatorTargetId == 5192826) then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/mox98012-ops/-./refs/heads/main/gitignore.lua"))();
+	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/86e73bccf9768a31edc644adc20feaf6.lua"))();
 elseif (tostring(game.PlaceId) == "155615604") then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/859516ffc2c6719d9fefdc7cdc30b8c6.lua"))();
 end;
